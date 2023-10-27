a7e73a1d0777f209e008241f9488316faf2f2cec
222122 <222122@WINMAC7>
2023-10-27 14:29:41 +0300

    initialization of project

---
+++ TechnicalShop/TechnicalShop/Baseы/Me.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TechnicalShop.Baseы
+{
+    public partial class Product
+
+    {
+        public string CostDiscount
+        {
+            get
+            {
+                if (Discount == 0)
+                    return Cost.ToString("N0") + " ₽";
+                else
+                    return (Cost - (Cost * (decimal)Discount / 100)).ToString("N0") + " ₽  ";
+            }
+        }
+        public Visibility CostVisibility
+        {
+            get
+            {
+                if (Discount == 0)
+                    return Visibility.Collapsed;
+                else
+                    return Visibility.Visible;
+            }
+        }
+
+        public string OverrideFeedback
+        {
+            get
+            { 
+                double sum = 0;
+
+                foreach(var item in Feedback)
+                {
+                    sum += item.Evaluation;
+                }
+                if(Feedback.Count() >= 11 && Feedback.Count() <= 19)
+                    return $"★ {(sum / Feedback.Count()).ToString("N2")}  {Feedback.Count()} отзывов";
+                else if(Feedback.Count() == 1 || Feedback.Count() % 10 == 1)
+                    return $"★ {(sum / Feedback.Count()).ToString("N2")}  {Feedback.Count()} отзыв";
+                else if (Feedback.Count() % 10 >= 2 || Feedback.Count() % 10 <= 4)
+                    return $"★ {(sum / Feedback.Count()).ToString("N2")}  {Feedback.Count()} отзывa";
+                else 
+                    return $"★ {(sum / Feedback.Count()).ToString("N2")}  {Feedback.Count()} отзывов";
+
+            }
+        }
+
+        public string GetImage
+        {
+            get
+            {
+                string g = "\u005c" + MainImagePath.Replace(' ', '\0').Replace('/', '\u005c');
+
+                return g;
+            }
+        }
+
+    }
+}
---
+++ TechnicalShop/TechnicalShop/Components/MeUserControl.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TechnicalShop.Baseы;
+
+namespace TechnicalShop.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для ItemUserControl.xaml
+    /// </summary>
+    public partial class MeUserControl : UserControl
+    {
+        public MeUserControl(Product product)
+        {
+            InitializeComponent();
+            //if(product.MainImagePath == null)
+
+            if(product.MainImage == null)
+                MainImage.Source = new BitmapImage(new Uri(@"/Resources/id_114593.1.jpg", UriKind.Relative));
+            else
+                MainImage.Source = GetImage(product.MainImage);
+            TitleTb.Text = product.Title;
+            NewPrice.Text = product.CostDiscount;
+            OldPrice.Visibility = product.CostVisibility;
+            EvalutionTb.Text = product.OverrideFeedback;
+            OldPrice.Text = product.Cost.ToString("N0");
+            BasketBt.Source = new BitmapImage(new Uri(@"/Resources/Basket.png", UriKind.Relative));
+            LikeBt.Source = new BitmapImage(new Uri(@"/Resources/Heart.png", UriKind.Relative));
+            StatisticBt.Source = new BitmapImage(new Uri(@"/Resources/Statistik.png", UriKind.Relative));
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private BitmapImage GetImage(byte[] byteImage)
+        {
+            if (byteImage != null)
+            {
+                MemoryStream byteStream = new MemoryStream(byteImage);
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.StreamSource = byteStream;
+                image.EndInit();
+                return image;
+            }
+            return null;
+        }
+    }
+}
